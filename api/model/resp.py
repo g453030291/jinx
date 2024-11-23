caf1129e29f9d0884ecf77ea1ae103970363549c
g453030291 <453030291@qq.com>
2024-11-23 23:51:39 +0800

    add:fictional face

--- api/model/resp.py
+++ api/model/resp.py
@@ -9,8 +9,8 @@ class Resp(BaseModel):
 
     @classmethod
     def success(cls, data: Optional[Any] = None) -> "Resp":
-        return cls(code=200, msg="Success", data=data)
+        return cls(code=200, msg="success", data=data)
 
     @classmethod
-    def fail(cls, msg: str = "Failure", code: int = 400, data: Union[Dict[str, Any], None] = None) -> "Resp":
+    def fail(cls, msg: str = "failure", code: int = 400, data: Union[Dict[str, Any], None] = None) -> "Resp":
         return cls(code=code, msg=msg, data=data)
--- api/routers/image.py
+++ api/routers/image.py
@@ -12,7 +12,7 @@ from api.model.resp import Resp
 
 router = APIRouter()
 
-# 生成一个图片处理的方法
+# 边缘引导图
 @router.post("/foreground/generate", response_model=Resp)
 def foreground_edges_generate(o_url: str = Body(..., embed=True)):
     hed_processor = Processor('softedge_hed')
@@ -38,5 +38,25 @@ def foreground_edges_generate(o_url: str = Body(..., embed=True)):
     hed_img.save(output, format='PNG')
     output.seek(0)
     oss_url = oss_client.put_object(str(uuid.uuid4()).replace('-', '') + '.png', output.read())
+
+    return Resp.success(data=oss_url)
+
+# 生成虚拟人脸图
+@router.get("/fictional/face", response_model=Resp)
+def fictional_face():
+    # 获取虚拟人物的图片
+    response = requests.get("https://thispersondoesnotexist.com")
+    response.raise_for_status()
+    picture = response.content
+
+    # 生成文件名并保存图片
+    file_name = uuid.uuid4().hex + ".jpeg"
+    with open(file_name, "wb") as f:
+        f.write(picture)
+
+    # 上传图片到 OSS 并获取 URL
+    oss_client = OSSClient()
+    with open(file_name, "rb") as f:
+        oss_url = oss_client.put_object(file_name, f.read())
 
     return Resp.success(data=oss_url)

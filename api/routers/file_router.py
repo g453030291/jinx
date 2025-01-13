822729de60542cdaf78c1869a3f8866e2a679ae0
g453030291 <453030291@qq.com>
2025-01-13 23:02:26 +0800

    bugfix:file name uuid

--- api/routers/file_router.py
+++ api/routers/file_router.py
@@ -1,3 +1,5 @@
+import uuid
+
 from fastapi import FastAPI, UploadFile, File, APIRouter
 
 from api.client.oss_client import OSSClient
@@ -11,7 +13,8 @@ async def upload(files: list[UploadFile] = File(...)):
     urls = []
     oss_client = OSSClient()
     for file in files:
+        file_name = f"{uuid.uuid4().hex}{file.filename[file.filename.rfind('.'):]}"
         contents = await file.read()
-        filename = file.filename
-        urls.append(oss_client.put_object(filename, contents))
+        # filename = file.filename
+        urls.append(oss_client.put_object(file_name, contents))
     return Resp.success(data={"urls": urls})
